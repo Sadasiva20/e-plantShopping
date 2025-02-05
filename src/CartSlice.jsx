298eb6fe8d2f0aa6bead4565f05a1aa0534e13ad
Sadasiva20 <sssank2@gmail.com>
2025-02-05 14:58:17 -0500

    update

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,17 +7,28 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-
-      
-    
-    },
+      addItem: (state, action) => {
+        const { name, image, cost } = action.payload;
+        const existing_Item = state.items.find(item_name => item_name.name === name);
+        if (existing_Item) {
+          existing_Item.quantity++;
+        } else {
+          state.items.push({ name, image, cost, quantity: 1 });
+        }
+      }},
   
     removeItem: (state, action) => {
 
-     
+      state.items = state.items.filter(item_name => item_name.name !== action.payload);
         }
     },
     updateQuantity: (state, action) => {
+
+      const { name, quantity } = action.payload;
+      const Update_item = state.items.find(item_name => item_name.name === name);
+      if (Update_item) {
+          Update_item.quantity = quantity;
+}
      
     },
 });
